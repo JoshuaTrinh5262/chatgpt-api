ace523983313d263fe3ec77b8fe3f1bf1dfa9d83
Trịnh Quang Trường <103560993+JoshuaTrinh5262@users.noreply.github.com>
2023-05-24 13:48:33 +0700

    Update 03_chatgpt_website.py
    store_conversation in chat_history.txt

--- 03_chatgpt_website.py
+++ 03_chatgpt_website.py
@@ -1,26 +1,32 @@
 import openai
 import gradio
+import api
 
-#Joshua's API Key
-openai.api_key = "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"
+openai.api_key = "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"
+
+# Open the text file in read mode
+with open("chat_history.txt", "r") as file:
+    # Read all lines from the file into a list
+    lines = file.readlines()
 
 #load chat_history here
-store_conversation = [
-    "User: Good Morning",
-    "User: Your Name Now is Anna",
-    "User: Your Age is Now 25",
-    "User: Your Favorite color is Pink",
-    "User: Your Like video game",
-]
+store_conversation = [line.strip() for line in lines]
+
+print(store_conversation)
 
 def CustomChatGPT(user_input):
-    store_conversation.append("User: " + user_input)
+    store_conversation.append("User1: " + user_input)
     response = openai.Completion.create(
         model = "text-davinci-003",
-        prompt = "\n".join(store_conversation)
+        prompt = "\n".join(store_conversation),
+        max_tokens = 50,
+        n = 1,
+        stop = None,
+        temperature = 0.5,
     )
+
     ChatGPT_reply = response.choices[0].text.strip()
-    store_conversation.append("AI: " + ChatGPT_reply)
+    store_conversation.append(ChatGPT_reply)
     saveChatHistory(store_conversation)
     return ChatGPT_reply
 
